2e8ab84683623fb295a122e83d8ca9228704ceef
morinota <morinota416@gmail.com>
2022-12-06 18:18:38 +0900

    コマンドの誤植を修正

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ from setuptools import find_packages, setup
 setup(name="src", version="0.1", packages=find_packages())
 
 # 参考: https://zenn.dev/karaage0703/articles/db8c663640c68b
-# setup.pyが存在する場所でpip install . -eを実行してください
+# setup.pyが存在する場所でpip install -e .を実行してください。
